2f089595d6c87280bc81acbd5e05c8a48ee3daae
Volt <72139731+VoltButOnGithub@users.noreply.github.com>
2025-07-04 11:53:14 +0200

    Update Welcome.vue

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -106,6 +106,20 @@ import { Head, Link } from '@inertiajs/vue3';
                                 wow i added this and just ran ./automation/deploy in the console and now its here!! big if true
                             </span>
                         </li>
+                        <li
+                            class="relative flex items-center gap-4 py-2 before:absolute before:top-0 before:bottom-1/2 before:left-[0.4rem] before:border-l before:border-[#e3e3e0] dark:before:border-[#3E3E3A]"
+                        >
+                            <span class="relative bg-white py-1 dark:bg-[#161615]">
+                                <span
+                                    class="flex h-3.5 w-3.5 items-center justify-center rounded-full border border-[#e3e3e0] bg-[#FDFDFC] shadow-[0px_0px_1px_0px_rgba(0,0,0,0.03),0px_1px_2px_0px_rgba(0,0,0,0.06)] dark:border-[#3E3E3A] dark:bg-[#161615]"
+                                >
+                                    <span class="h-1.5 w-1.5 rounded-full bg-[#dbdbd7] dark:bg-[#3E3E3A]" />
+                                </span>
+                            </span>
+                            <span>
+                                wow i added this and just ran ./automation/deploy-if-new in the console and now its here!! very big if true
+                            </span>
+                        </li>
                     </ul>
                     <ul class="flex gap-3 text-sm leading-normal">
                         <li>
