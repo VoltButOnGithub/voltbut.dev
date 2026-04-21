8dabcc02b6a1004f9a03253509e7016c06d98efc
Volt <72139731+VoltButOnGithub@users.noreply.github.com>
2026-04-21 20:21:27 +0200

    Update Welcome.vue

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -90,8 +90,19 @@ import { Head, Link } from '@inertiajs/vue3';
                             <span>
                                 Im going to blow this website up soon its a stinky Larvel Inertia Vue project i hate it its so smelly so big and gassy ugh. Gonna replace it with a CLEAN Nuxt project at some point
                             </span>
+                        </li>
+                        <li
+                            class="relative flex items-center gap-4 py-2 before:absolute before:top-0 before:bottom-1/2 before:left-[0.4rem] before:border-l before:border-[#e3e3e0] dark:before:border-[#3E3E3A]"
+                        >
+                            <span class="relative bg-white py-1 dark:bg-[#161615]">
+                                <span
+                                    class="flex h-3.5 w-3.5 items-center justify-center rounded-full border border-[#e3e3e0] bg-[#FDFDFC] shadow-[0px_0px_1px_0px_rgba(0,0,0,0.03),0px_1px_2px_0px_rgba(0,0,0,0.06)] dark:border-[#3E3E3A] dark:bg-[#161615]"
+                                >
+                                    <span class="h-1.5 w-1.5 rounded-full bg-[#dbdbd7] dark:bg-[#3E3E3A]" />
+                                </span>
+                            </span>
                             <span>
-                                Tribute to kerret here hes miku or something idk bru
+                                Tribute to kerret here hes miku or something
                             </span>
                         </li>
                     </ul>
